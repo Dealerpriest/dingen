bc1fc351ec99e29a9d2eaba0cfae29588e5585d4
HACKING-STATION\gunhaxxor <gunnar.oledal@gmail.com>
2019-07-19 10:33:00 +0200

    Mwhohahahaha.

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import '@mdi/font/css/materialdesignicons.css'
-import './registerServiceWorker';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "@mdi/font/css/materialdesignicons.css";
+import "./registerServiceWorker";
 import Vuetify from "vuetify";
-import 'vuetify/dist/vuetify.min.css';
-import './register-hooks';
+import "vuetify/dist/vuetify.min.css";
+import "./register-hooks";
 
 Vue.use(Vuetify, {
-  iconfont: 'mdi'
-})
+  iconfont: "mdi"
+});
 
-import Parse from 'parse';
+import Parse from "parse";
 
 Parse.serverURL = "https://storage.tiigbg.se:4444/parse";
-Parse.initialize('dingelingen');
+Parse.initialize("dingelingen");
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = true;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app");
