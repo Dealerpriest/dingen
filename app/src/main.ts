f5e803d4e625c8604b0ca1e68e84e2bced8bff9e
Gunnar Oledal <gunnar.oledal@gmail.com>
2021-09-27 18:58:49 +0200

    slowly transitioning towards a docker setup

--- app/src/main.ts
+++ app/src/main.ts
@@ -14,8 +14,16 @@ Vue.use(Vuetify, {
 
 import Parse from "parse";
 
-Parse.serverURL = "https://storage.tiigbg.se:4444/parse";
-Parse.initialize("dingelingen");
+// Parse.serverURL = "https://storage.tiigbg.se:4444/parse";
+// Parse.initialize("dingelingen");
+Parse.serverURL = `${process.env.VUE_APP_BACKEND_SERVER}:${process.env.VUE_APP_PARSE_PORT}${process.env.VUE_APP_PARSE_URL_PATH}`
+if(!process.env.VUE_APP_PARSE_APP_ID){
+  console.error('No parse app id provided');
+} else {
+  Parse.initialize(process.env.VUE_APP_PARSE_APP_ID);
+}
+
+console.log(process.env);
 
 Vue.config.productionTip = true;
 
